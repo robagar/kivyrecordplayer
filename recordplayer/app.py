1a14a0e715ee45c1aa4cd53dd52cd4d832ef36a8
robagar <robagar@localhost>
2017-06-25 16:32:14 +0100

    toolbar stub

--- recordplayer/app.py
+++ recordplayer/app.py
@@ -2,10 +2,12 @@ from kivy.app import App
 from kivy.logger import Logger
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.popup import Popup
 
 from . import settings
 from .player import create_player
@@ -21,11 +23,93 @@ class RecordWidget(ButtonBehavior, Image):
 class RecordPlayerApp(App):
 
     def build(self):
-        ac = self.album_container = StackLayout(spacing=10, size_hint_y=None)
-        ac.bind(minimum_height=ac.setter('height'))
-        root = ScrollView()
-        root.add_widget(ac)
+        root = BoxLayout(orientation='vertical')
+        root.add_widget(self.init_albums_view());
+        root.add_widget(self.init_toolbar());
         return root
+
+    def init_albums_view(self):
+        ac = self.album_container = StackLayout(
+            padding=10,
+            spacing=10#, 
+            # size_hint_y=None
+        )
+        # ac.bind(minimum_height=ac.setter('height'))
+        v = ScrollView()
+        v.add_widget(ac)
+        return v
+
+    def init_toolbar(self):
+        tb = self.toolbar = BoxLayout(
+            size_hint_y=None,
+            height=50,
+            padding=5,
+            spacing=5,
+            orientation='horizontal'
+        )
+
+        # previous track
+        self.prev_button = Button(
+            text="prev",
+            size_hint_x=None,
+            width=100,
+            on_press=self.on_prev_button_press
+        )
+        tb.add_widget(self.prev_button)
+
+        # pause / resume
+        self.pause_button = Button(
+            text="pause",
+            size_hint_x=None,
+            width=100,
+            on_press=self.on_pause_button_press
+        )
+        tb.add_widget(self.pause_button)
+        
+        # next track
+        self.next_button = Button(
+            text="next",
+            size_hint_x=None,
+            width=100,
+            on_press=self.on_next_button_press
+        )
+        tb.add_widget(self.next_button)
+        
+        # currently playing
+        self.playing_label = Label(
+            text="(not playing)"
+        )
+        tb.add_widget(self.playing_label)
+
+        # system popup
+        self.init_system_popup()
+        tb.add_widget(Button(
+            text='system',
+            size_hint_x=None,
+            width=100,
+            on_press=self.system_popup.open
+        ))
+        return tb
+
+    def init_system_popup(self):
+        c = BoxLayout(
+            orientation='vertical',
+            padding=10,
+            spacing=10
+        )
+        c.add_widget(Button(
+            text='shutdown',
+            on_press=self.on_shutdown_press
+        ))
+        c.add_widget(Button(
+            text='reboot',
+            on_press=self.on_reboot_press
+        ))
+        self.system_popup = Popup(
+            title='Record Player',
+            size_hint=(0.5, 0.5),
+            content=c
+        )
 
     def on_start(self):
         Logger.info('START')
@@ -53,3 +137,18 @@ class RecordPlayerApp(App):
             p.resume()
         else:    
             p.play_album(a)
+
+    def on_prev_button_press(self, widget):
+        pass
+
+    def on_next_button_press(self, widget):
+        pass
+
+    def on_pause_button_press(self, widget):
+        pass
+
+    def on_shutdown_press(self, widget):
+        pass
+
+    def on_reboot_press(self, widget):
+        pass
