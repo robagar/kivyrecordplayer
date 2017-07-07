14bbc5a6b78ca53a9f4c4730a353cf13191cefe2
robagar <robagar@localhost>
2017-07-07 09:25:17 +0100

    screen power test

--- recordplayer/devices/raspberrypi.py
+++ recordplayer/devices/raspberrypi.py
@@ -1,7 +1,8 @@
-from rpi_backlight import set_brightness
+import rpi_backlight as bl
 from ..device import Device
 
 
 class RaspberryPiDevice(Device):
     def set_screen_brightness(self, brightness):
-        set_brightness(round(brightness * 255), smooth=True, duration=3)    
+        bl.set_power(brightness > 0.5)
+        # bl.set_brightness(round(brightness * 255), smooth=True, duration=3)    
